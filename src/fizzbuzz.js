2a06f0ad5213a3a58622d11c7e292b98efb524f5
Carlos Ortiz <carlos72ortiz14@gmail.com>
2025-08-25 12:31:41 -0600

    El programa ya imprime FizzBuzz en multiplos de 3 y 5, tambien Fizz en multiplos de 3 y Buzz en multiplos de 5

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -5,6 +5,12 @@ export function fizzbuzz(){
     if (num % 3 == 0 && num %5==0) {
       console.log("FizzBuzz")
     }
+    else if (num % 3 == 0){
+      console.log("Fizz")
+    }
+    else if (num % 5 ==0){
+      console.log("Buzz")
+    }
     else {
       console.log(num)
     }
